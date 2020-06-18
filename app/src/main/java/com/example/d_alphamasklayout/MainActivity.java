2bb9fb40ad2768903948dcb202cae7b3b4158956
hank <%pink236545>
2020-06-18 13:22:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/d_alphamasklayout/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.d_alphamasklayout;
+//implementation 'com.zaaach:alphamasklayout:1.1' 設定陰影
+//目的:點選按鈕跳出popupWindow在指定Layout顯示遮罩,當點炫後離開遮罩
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.PopupWindow;
+import android.widget.Toast;
+
+import com.zaaach.alphamasklayout.AlphaMaskLayout;
+
+public class MainActivity extends AppCompatActivity {
+    private AlphaMaskLayout maskLayout;
+    private ImageView moreBtn;
+    private PopupWindow mPopupWindow;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        moreBtn = (ImageView) findViewById(R.id.more);
+        moreBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View content = LayoutInflater.from(MainActivity.this).inflate(R.layout.popup, null);
+                mPopupWindow = new PopupWindow(content, 300, 400);
+                mPopupWindow.setFocusable(true);
+                mPopupWindow.setOutsideTouchable(true);
+                mPopupWindow.setBackgroundDrawable(new ColorDrawable());
+                mPopupWindow.showAsDropDown(moreBtn);
+                mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                    @Override
+                    public void onDismiss() {
+                        maskLayout.hideMask();
+                    }
+                });
+                maskLayout.showMask();
+            }
+        });
+
+        maskLayout = (AlphaMaskLayout) findViewById(R.id.mask_layout);
+        //you can also do like this:
+//        maskLayout.setAlphaFrom(0);
+//        maskLayout.setAlphaTo(127);
+//        maskLayout.setDuration(600);
+
+        maskLayout.setOnAlphaFinishedListener(new AlphaMaskLayout.OnAlphaFinishedListener() {
+            @Override
+            public void onShowFinished() {
+                Toast.makeText(MainActivity.this, "show finished", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onHideFinished() {
+                Toast.makeText(MainActivity.this, "hide finished", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
